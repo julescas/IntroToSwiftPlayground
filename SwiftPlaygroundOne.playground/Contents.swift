55b1298a8860cfa60c3cc3c1212ac5756f07e159
julescas <112312841+julescas@users.noreply.github.com>
2022-09-07 16:54:51 -1000

    Boolean True or False

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
 var greeting = "Hello, playground"
+// Assignment #2 Introduction to True and False
+var appState = false
+var numberOne = 5
+var numberTwo = 7
+var comparison = numberOne < numberTwo
+var comparison = numberOne > numberTwo
+
