c60222e18f38a5f351e19460df251c45f3d9bc7c
julescas <112312841+julescas@users.noreply.github.com>
2022-09-07 18:02:11 -1000

    Completed Swift Playgrounds #1

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
 var greeting = "Hello, playground"
+var myFirstVariable: String
+myFirstVariable = "I've created my first variable"
+var mySecondVariable = "and look I added 2 strings together"
+var myThirdVariable = myFirstVariable + mySecondVariable
+print(myThirdVariable)
+var myOwnVariable: String
+myOwnVariable = "Hello My Name is Jules"
+var myOwnVariableTwo: String
+myOwnVariableTwo = "I like mango"
+var myOwnVariableThree: String
+myOwnVariableThree = "and I'm going to add 3 strings together"
+var myOwnVariableFour = myOwnVariable + myOwnVariableTwo + myOwnVariableThree
+print(myOwnVariableFour)
+
+
